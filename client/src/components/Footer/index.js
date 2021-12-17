1847d04bc89413ad9baaeb3256e2af3c705730bb
Morris <yiiyatechuganda@gmail.com>
2021-12-17 15:51:32 +0300

    No database found

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,8 +7,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto text-dark p-4">
       <div className="container text-center mb-5">
-        <h4 className='footer-font'>&copy; {new Date().getFullYear()} - World Gospel Ministries.</h4>
-        <h4><br/></h4>
+        <h4 className='footer-font'>&copy; {new Date().getFullYear()} - World Gospel Ministries.</h4><br/>
       </div>
     </footer>
   );
