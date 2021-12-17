be8d51c1f583718f60eea7ff125ab9a2eff9f8a8
Morris <yiiyatechuganda@gmail.com>
2021-12-17 15:31:52 +0300

    No database found

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto text-dark p-4">
       <div className="container text-center mb-5">
-        <h4 className='footer-font'>&copy; {new Date().getFullYear()} - Morisoft Inc.</h4>
+        <h4 className='footer-font'>&copy; {new Date().getFullYear()} - World Gospel Ministries.</h4>
       </div>
     </footer>
   );
