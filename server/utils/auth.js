92b824907e8101c5df4206beb8d9cc1433016393
Morris <yiiyatechuganda@gmail.com>
2021-12-17 16:50:28 +0300

    No database found

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
-
-const secret = process.env.JWT_SECRET;
+const secret ='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImsxMjM0NSJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vcnJpcyBUaGFua3MiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyfQ.QT9rT-Vao6kBcXpWk7P-PqDmQb-nx7PN2NvbwttMzEKJN8fUEneWVqvuqR8x0bttcYsJtviJJcJf8-Rm4cm3Bp8g4MDI9k_M_mRkk7ixW9pZUpYdcsJnfj7K-8KFEiDXnGSa9SjFzbA3wYRsvFZSy9M_3rP_tv4mpBCZFGmHsUFZyzEcrgoo3ZlADQu9uGHHY8lulO2Mkb5-eMBkUKBxLppQv7pEsTp3CZm3bN_iSNgXCPx2vA03TESwMb7Bip0V6MDsvDGbaCz6sSxKtusUCd8Nj9CtHNzPZRkjZm0ToJcYkB6OsCv_YzWZC_ncMDFlLC8SU2M6HSFG0UBWKEqaeA'
 const expiration = '2h';
 
 module.exports = {
